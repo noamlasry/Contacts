7a2a3a34b410433a7fd13717d586428963f5cba8
Arkanoid <noamlasry02@gmail.com>
2019-12-21 16:58:41 +0200

    add icon to list view

--- app/src/main/java/com/noamls_amirbs/ex3/MainActivity.java
+++ app/src/main/java/com/noamls_amirbs/ex3/MainActivity.java
@@ -1,21 +1,29 @@
 package com.noamls_amirbs.ex3;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
@@ -27,31 +35,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private SQLiteDatabase contactsDB = null;
     public static final String MY_DB_NAME = "contacts.db";
 
+    int images[] = {R.mipmap.gray_phone_icon,R.mipmap.green_phone_icon};
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.lstViewID);
-        insert_btn = (Button)findViewById(R.id.insert_id);
-        search_btn = (Button)findViewById(R.id.search_id);
-        nameField = (EditText)findViewById(R.id.name_id);
-        phoneField = (EditText)findViewById(R.id.phone_id);
+        insert_btn = (Button) findViewById(R.id.insert_id);
+        search_btn = (Button) findViewById(R.id.search_id);
+        nameField = (EditText) findViewById(R.id.name_id);
+        phoneField = (EditText) findViewById(R.id.phone_id);
 
         insert_btn.setOnClickListener(this);
         search_btn.setOnClickListener(this);
+        listView = findViewById(R.id.lstViewID);
 
-
-
-        arrayList = new ArrayList<>();
-        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
-        listView.setAdapter(arrayAdapter);
         createDB();
-        showContacts();
+        displayContact();
 
 
+    }
+    public void displayContact()
+    {
+        Vector<String> mTitle = new Vector<>();
+        mTitle = showContacts();
+        MyAdapter adapter = new MyAdapter(this, mTitle, images);
+        listView.setAdapter(adapter);
+        Log.d("debug", "here!!!!");
+        for (int i = 0; i < mTitle.size(); i++)
+        {
+            Log.d("debug","valu: "+ mTitle.get(i));
+        }
+    }
+    class MyAdapter extends ArrayAdapter<String> {
 
+        Context context;
+        Vector<String> rTitle;
+        int rImgs[];
+
+        MyAdapter (Context c, Vector<String> title, int imgs[]) {
+            super(c, R.layout.row, R.id.textView1, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rImgs = imgs;
+
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
+            ImageView images = row.findViewById(R.id.image);
+            TextView myTitle = row.findViewById(R.id.textView1);
+
+
+            // now set our resources on views
+            images.setImageResource(rImgs[0]);
+            myTitle.setText(rTitle.get(position));
+
+
+            return row;
+        }
     }
 
     @Override
@@ -94,48 +142,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         String sql = "INSERT INTO contacts (name, phone) VALUES ('" + contactName + "', '" + contactPhone + "');";
         contactsDB.execSQL(sql);
-        arrayList.add(contactName + "  " + contactPhone);
-        arrayAdapter.notifyDataSetChanged();
         Toast.makeText(this, contactName + "one contact added", Toast.LENGTH_SHORT).show();
+
+        displayContact();
     }
 
-    public void showContacts()
+    public Vector<String> showContacts()
     {
-        // A Cursor provides read and write access to database results
+        Vector <String> vec = new Vector<>();
         String sql = "SELECT * FROM contacts";
         Cursor cursor = contactsDB.rawQuery(sql, null);
 
-        // Get the index for the column name provided
         int idColumn = cursor.getColumnIndex("id");
         int nameColumn = cursor.getColumnIndex("name");
         int phoneColumn = cursor.getColumnIndex("phone");
 
         String contactList = "";
-
-        // Move to the first row of results & Verify that we have results
         if (cursor.moveToFirst()) {
             do {
                 contactList = "";
-                // Get the results and store them in a String
                 String id = cursor.getString(idColumn);
                 String name = cursor.getString(nameColumn);
                 String phone = cursor.getString(phoneColumn);
 
-                contactList = contactList  + name  + phone;
+                contactList = contactList  + name  +"  "+ phone ;
 
-                arrayList.add(contactList);
-                arrayAdapter.notifyDataSetChanged();
+                vec.addElement(contactList);
 
-                // Keep getting results as long as they exist
             } while (cursor.moveToNext());
-
 
 
         } else {
 
             Toast.makeText(this, "No Results to Show", Toast.LENGTH_SHORT).show();
         }
-
+        return vec;
     }
 
 }
